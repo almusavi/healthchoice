95b29004be27a6d0523ccc47e9ddb7919549784f
Alamdar Musavi <almusavi@gmail.com>
2017-04-17 16:43:54 -0500

    add insurance working

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,15 +1,20 @@
 class ProvidersController < ApplicationController
 
 def index
+	if params[:cityzip] == ""
+		@error = "You must enter a location"
+		render "welcome/index"
+	elsif params[:cityzip] != "" && params[:insurance][0] == ""
+		@providers = Provider.search(params[:cityzip])
+	elsif params[:cityzip] != "" && params[:insurance][0] != ""
+		@providers = Provider.search(params[:cityzip]).insurancesearch(params[:insurance][0].upcase)			
+	end
 	@search = params[:cityzip]
 	@insurancesearch = params[:insurance]
-
+	p "*****************"
+	p params
+	p "****************"
 	@locations = []
-	@providers = Provider.search(params[:cityzip])
-	@providersarray = []
-	if params[:insurance][0] != ""
-		@providers.reject {|provider| provider.insurancepanels.include? @insurancesearch[0].upcase}
-	end
 	@providers = @providers.paginate(:per_page => 5, :page => params[:page])
 end
 
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,8 +3,16 @@ class Provider < ApplicationRecord
 	acts_as_mappable :lat_column_name => :maillatitude, :lng_column_name => :maillongitude
 
 	def self.search(search)
-		if search
+		if search != ""
 			providers = self.within(5, :origin => search)	
+		else
+			self.all
+		end
+	end
+
+	def self.insurancesearch(insurancesearch)
+		if insurancesearch != ""
+			self.where("insurancepanels LIKE ?", "%#{insurancesearch}%").all	
 		else
 			self.all
 		end
