7660e1d6cf9f0a99d008445f085f316e2088c4bf
Alamdar Musavi <almusavi@gmail.com>
2017-06-10 12:40:06 -0500

    add error handling

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,16 +2,16 @@ class ProvidersController < ApplicationController
 	require 'will_paginate/array'
 	def index
 		if params[:cityzip] == "" && params[:searchname] == ""
+			p "***************"
 			@error = "You must enter a location"
-			render "welcome/index"
+			p "***************"
+			@providers = Provider.all
+			render'welcome/index.html.erb'
 		elsif params[:searchname] != "" 
 			@providers = Provider.searchname(params[:searchname].capitalize)
 		elsif params[:cityzip] != "" && params[:insurance][0] == "---"
 			@providers = Provider.search(params[:cityzip])
 		elsif params[:cityzip] != "" && params[:insurance][0] != ""
-			p "***************"
-			p params
-			p "***************"
 			@providers = Provider.search(params[:cityzip]).insurancesearch(params[:insurance][0].upcase)			
 		end
 		@searchname = params[:searchname]
@@ -34,48 +34,6 @@ class ProvidersController < ApplicationController
 			city["results"][0]["geometry"]["location"]
 		end
 
-	# 	def coor_dist(lat1, lon1, lat2, lon2)
-
-	# 	  earthRadius = 6371 # Earth's radius in KM
-
-	# 	  deltaLat = (lat2-lat1)
-	# 	  deltaLon = (lon2-lon1)
-	# 	  deltaLat = conv_deg_rad(deltaLat)
-	# 	  deltaLon = conv_deg_rad(deltaLon)
-
-	# 	  # Calculate square of half the chord length between latitude and longitude
-	# 	  a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
-	# 	      Math.cos((lat1/180 * Math::PI)) * Math.cos((lat2/180 * Math::PI)) *
-	# 	          Math.sin(deltaLon/2) * Math.sin(deltaLon/2);
-
-	# 	  # Calculate the angular distance in radians
-	# 	  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-
-	# 	  distance = earthRadius * c
-
-	# 	  distance
-	# 	end
-
-	# 	# convert degrees to radians
-	# 	def conv_deg_rad(value)
-	# 	  (value/180) * Math::PI unless value.nil? or value == 0
-	# 	end
-
-
-	# # puts coor_dist(lat1, lon1, lat2, lon2)
-
-	# 	def distance(city1, city2)
-	# 		url1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city1 + '&key=AIzaSyC_VdOLBtVFbR-6rRP5-hRFXChHa78bDmM'
-	# 		url2 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city2 + '&key=AIzaSyC_VdOLBtVFbR-6rRP5-hRFXChHa78bDmM'
-
-	# 	end
-	# 	@providers.each do |each_provider|
-	# 		lat1 = latlon(@search)["lat"]
-	# 		lng1 = latlon(@search)["lng"]
-	# 		each_provider.distance = coor_dist(lat1, lng1, each_provider.maillatitude, each_provider.maillongitude ).to_f.round(3)
-	# 		each_provider.save!
-	# 	end
-		# @providers = @providers.sort_by{|p| p.distance}
 		@providers = @providers.order("lastname")
 		@providers = @providers.order('distance asc').paginate(:per_page => 30, :page => params[:page])
 	end
